b6fb498f38554f6de73ab678da9294ced9b99ce7
Thanush Curtis <thanushcurtis@gmail.com>
2026-05-09 22:47:03 +0100

    Updates

---
+++ _next/static/chunks/2a7cfb437ca6f64a.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/ab33c017c7d0c40f.js",
+  "static/chunks/turbopack-7ef8ca41df3d606b.js"
+])
---
+++ _next/static/chunks/c024e7eded63d86d.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/ab33c017c7d0c40f.js",
+  "static/chunks/turbopack-181e224d9948923d.js"
+])
